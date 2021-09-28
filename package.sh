4e2b30ee94da258074b8e8395a8b9356336e9cfb
bas080 <bas080@hotmail.com>
2021-09-28 16:00:00 +0200

    Initiate project

---
+++ package.sh
@@ -0,0 +1,5 @@
+executable mache       https://raw.githubusercontent.com/bas080/mache/master/mache
+executable markatzea   https://raw.githubusercontent.com/bas080/markatzea/master/markatzea
+sourceable bash-tap    https://raw.githubusercontent.com/bas080/bash-tap/master/bash-tap
+linked     bashionista $HOME/projects/bashionista/bashionista # TODO: make it use the remote file.
+linked     package     $PWD/package # TODO: make it use the remote file.
