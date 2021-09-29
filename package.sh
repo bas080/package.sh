c16ce25f53dcbd9ebc3ca00c9d41735bb2329523
bas080 <bas080@hotmail.com>
2021-09-29 13:04:44 +0200

    Create template function
    Improves code reuse in flatten function.

--- package.sh
+++ package.sh
@@ -1,5 +1,18 @@
-executable mache       https://raw.githubusercontent.com/bas080/mache/master/mache
-executable markatzea   https://raw.githubusercontent.com/bas080/markatzea/master/markatzea
-sourceable bash-tap    https://raw.githubusercontent.com/bas080/bash-tap/master/bash-tap
-linked     bashionista $HOME/projects/bashionista/bashionista # TODO: make it use the remote file.
-linked     package     $PWD/package # TODO: make it use the remote file.
+package $HOME/projects/bashionista/package.sh
+
+executable mache          https://raw.githubusercontent.com/bas080/mache/master/mache
+executable markatzea      https://raw.githubusercontent.com/bas080/markatzea/master/markatzea
+sourceable bash-tap       https://raw.githubusercontent.com/bas080/bash-tap/master/bash-tap
+linked     package        $PWD/package
+
+linked ls
+linked wget
+linked chmod
+linked ln
+linked rm
+linked bash
+linked mkdir
+linked which
+linked readlink
+linked mv
+linked cat
